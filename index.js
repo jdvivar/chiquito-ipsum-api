0a21117886e07fd7c8ba1d027ec69cb6017259fb
Daniel Vivar <jdvivar@gmail.com>
2020-02-21 18:22:24 +0100

    Add max paras and warning

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const { getChiquitoIpsum } = require('./chiquito-ipsum')
 
 app.get('/:paras?/:mode?', function ({ params: { mode, paras } }, res) {
   console.log(`received a request with mode ${mode} and ${paras}`)
+  if (paras > 10) {
+    console.warn("API doesn't allow more than 10 paragraphs")
+    paras = 10
+  }
   res.send(getChiquitoIpsum({ mode, paras }))
 })
 
